8b6ddd5e6071a09d9efb767abb30fec5f2b935dc
malikmustafaa <119024971+malikmustafaa@users.noreply.github.com>
2024-02-22 15:00:46 +0700

    menambahkan database
    database gallery_spk3

--- gallery.sql
+++ gallery.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 20 Feb 2024 pada 03.48
+-- Waktu pembuatan: 22 Feb 2024 pada 08.54
 -- Versi server: 10.1.40-MariaDB
 -- Versi PHP: 7.3.5
 
@@ -42,7 +42,11 @@ CREATE TABLE `album` (
 
 INSERT INTO `album` (`AlbumID`, `NamaAlbum`, `Deskripsi`, `TanggalDiBuat`, `UserID`) VALUES
 (1, 'Mobil', 'Semua Mobil                                    ', '2024-02-19', 1),
-(2, 'Motor', 'Semua Motor', '2024-02-19', 1);
+(2, 'Motor', 'Semua Motor', '2024-02-19', 1),
+(3, 'Kucing', 'Semua kucing', '2024-02-21', 2),
+(4, 'Laptop', 'Semua Laptop', '2024-02-21', 2),
+(5, 'Hanphone', 'Semua hanphone', '2024-02-21', 3),
+(6, 'Kapal', 'Semua Kapal', '2024-02-21', 3);
 
 -- --------------------------------------------------------
 
@@ -68,7 +72,15 @@ INSERT INTO `foto` (`FotoID`, `JudulFoto`, `DeskripsiFoto`, `TanggalUnggah`, `Lo
 (1, 'Mobil', 'Lamborgini', '2024-02-18', '1030269340-mobil1.jpg', 1, 1),
 (2, 'Mobil', 'Roll Royce', '2024-02-18', '1346634465-mobil2.jpg', 1, 1),
 (5, 'Motor', 'Fizr', '2024-02-20', '97450541-ð—™ðŸ­ð˜‡ð—¥.jpg', 2, 1),
-(6, 'Motor', 'Aerox', '2024-02-20', '637649793-download (14).jpg', 2, 1);
+(6, 'Motor', 'Aerox', '2024-02-20', '637649793-download (14).jpg', 2, 1),
+(7, 'Kucing', 'Kucing anggora', '2024-02-21', '51831843-OIP.jpg', 3, 2),
+(8, 'Kucing', 'Kucing oyen', '2024-02-21', '484955163-kucing2.jpg', 3, 2),
+(9, 'Laptop', 'Macbook Pro 13', '2024-02-21', '578612534-pro2016_MAIN_115.png', 4, 2),
+(10, 'Laptop', 'Asus ROG Flow X13', '2024-02-21', '1869729233-2021-03-18-product-7.webp', 4, 2),
+(11, 'Hanphone', 'Iphone 15 pro max', '2024-02-21', '1331294228-OIP (1).jpg', 5, 3),
+(12, 'Hanphone', 'Rog', '2024-02-21', '930058473-1671266-1000xauto-harga-hp-asus-rog-phone-generasi-pertama-hingga-terbaru-idaman-gamer.jpg', 5, 3),
+(13, 'Kapal', 'Kapal Pesiar', '2024-02-21', '726536675-R.jpg', 6, 3),
+(14, 'Kapal', 'Kapal selam', '2024-02-21', '1677239574-Kapal Selam.jpg', 6, 3);
 
 -- --------------------------------------------------------
 
@@ -89,11 +101,11 @@ CREATE TABLE `komentarfoto` (
 --
 
 INSERT INTO `komentarfoto` (`KomentarID`, `FotoID`, `UserID`, `IsiKomentar`, `TanggalKomentar`) VALUES
-(2, 1, 1, 'ASDASDAS', '2024-02-18'),
-(3, 1, 1, 'MOBIL YANG MAHAL', '2024-02-18'),
-(39, 1, 1, 'DASDAS', '2024-02-19'),
-(40, 2, 1, 'mobil impian', '2024-02-19'),
-(41, 1, 1, 'halo semua', '2024-02-20');
+(45, 1, 2, 'mobil yang mahal', '2024-02-21'),
+(46, 2, 2, 'mobil mahal', '2024-02-21'),
+(47, 5, 2, 'motor impian', '2024-02-21'),
+(48, 6, 2, 'keren banget', '2024-02-21'),
+(49, 7, 2, 'imut banget', '2024-02-21');
 
 -- --------------------------------------------------------
 
@@ -113,8 +125,15 @@ CREATE TABLE `likefoto` (
 --
 
 INSERT INTO `likefoto` (`LikeID`, `FotoID`, `UserID`, `TanggalLike`) VALUES
-(41, 2, 1, '2024-02-19'),
-(44, 1, 1, '2024-02-19');
+(46, 6, 1, '2024-02-21'),
+(54, 5, 1, '2024-02-21'),
+(61, 1, 1, '2024-02-21'),
+(63, 2, 1, '2024-02-21'),
+(64, 1, 2, '2024-02-21'),
+(65, 2, 2, '2024-02-21'),
+(66, 5, 2, '2024-02-21'),
+(67, 6, 2, '2024-02-21'),
+(69, 7, 2, '2024-02-21');
 
 -- --------------------------------------------------------
 
@@ -136,7 +155,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`UserID`, `Username`, `Password`, `Email`, `NamaLengkap`, `Alamat`) VALUES
-(1, 'malik', '827ccb0eea8a706c4c34a16891f84e7b', 'malikmustafa72735@gmail.com', 'malik ma', 'hagu selatan');
+(1, 'malik', '827ccb0eea8a706c4c34a16891f84e7b', 'malikmustafa72735@gmail.com', 'malik ma', 'hagu selatan'),
+(2, 'as', 'c20ad4d76fe97759aa27a0c99bff6710', 'fahrialgi275@gmail.com', 'azril ramadhan', 'hagu teungoh'),
+(3, 'satria', 'c20ad4d76fe97759aa27a0c99bff6710', 'satriaramadhan@gmail.com', 'Satria Ramadhan', 'Kandang');
 
 --
 -- Indexes for dumped tables
@@ -187,31 +208,31 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `album`
 --
 ALTER TABLE `album`
-  MODIFY `AlbumID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `AlbumID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `foto`
 --
 ALTER TABLE `foto`
-  MODIFY `FotoID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `FotoID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT untuk tabel `komentarfoto`
 --
 ALTER TABLE `komentarfoto`
-  MODIFY `KomentarID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `KomentarID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT untuk tabel `likefoto`
 --
 ALTER TABLE `likefoto`
-  MODIFY `LikeID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `LikeID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
